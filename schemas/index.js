acb02ba73da32cd56ce6ad15564977137ab73f37
Ristan Alfatha Aditara <ristanaa09@gmail.com>
2022-11-23 17:16:00 +0800

    connect atlas

--- schemas/index.js
+++ schemas/index.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connect = () => {
   mongoose
-    .connect("mongodb://localhost:27017/week1_pt_2")
+    // .connect("mongodb://localhost:27017/week1_pt_2")
+    .connect("mongodb+srv://RistanAA:admin@cluster0.xldtokf.mongodb.net/?retryWrites=true&w=majority")
     .catch(err => console.log(err));
 };
 
